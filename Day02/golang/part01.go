19cf50abcb3e1f8e3227123b29b1f1b43fc048b1
Kevin Sonney <kevin.sonney@elastic.co>
2021-12-02 19:22:09 -0500

    Solution for Day 2 in golang

---
+++ Day02/golang/part01.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	var posX, posZ int = 0,0
+	inputFile, myBad := os.Open("../input.txt")
+	checkErr(myBad)
+	defer inputFile.Close()
+
+	inFile := bufio.NewScanner(inputFile)
+	for inFile.Scan() {
+		direction := strings.Split(inFile.Text()," ")
+		dist,myBad := strconv.ParseInt(direction[1],10,0)
+		checkErr(myBad)
+		switch dir := direction[0]; dir {
+		case "forward":
+			posX = posX + int(dist)
+		case "down":
+			posZ = posZ + int(dist)
+		case "up":
+			posZ = posZ - int(dist)
+		}
+	}
+	
+	fmt.Println("Depth is",posZ)
+	fmt.Println("Distance is", posX)
+	fmt.Println("Final Position is", (posX * posZ))
+	myBad = inFile.Err();
+	checkErr(myBad)
+
+}
---
+++ Day02/golang/part02.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func checkErr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	var posX, posZ, aim int = 0,0,0
+	inputFile, myBad := os.Open("../input.txt")
+	checkErr(myBad)
+	defer inputFile.Close()
+
+	inFile := bufio.NewScanner(inputFile)
+	for inFile.Scan() {
+		direction := strings.Split(inFile.Text()," ")
+		dist,myBad := strconv.ParseInt(direction[1],10,0)
+		checkErr(myBad)
+		switch dir := direction[0]; dir {
+		case "forward":
+			posX = posX + int(dist)
+			posZ = posZ + (int(dist) * aim)
+		case "down":
+			aim = aim + int(dist)
+		case "up":
+			aim = aim - int(dist)
+		}
+		fmt.Println("Depth:",posZ,"Aim:",aim,"Distance:",posX)
+	}
+	
+	fmt.Println("Depth is",posZ)
+	fmt.Println("Distance is", posX)
+	fmt.Println("Final Position is", (posX * posZ))
+	myBad = inFile.Err();
+	checkErr(myBad)
+
+}
